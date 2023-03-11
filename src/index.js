8043efce9c211c6798a08c72687f9e284ca5ef55
Majeste0 <rom.pailhes@gmail.com>
2023-03-11 11:29:51 +0100

    Almost all CS

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles/style.scss";
+
+export const randomHexa = () => {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return "#" + n.slice(0, 6);
+  console.log(random_hex_color_code());
+};
