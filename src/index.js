069bd9bbba3487e2f2496bfb913bc5dabfbb40d4
Majeste0 <rom.pailhes@gmail.com>
2023-03-11 11:50:33 +0100

    Added JS with func RandomHeax + randomizeColorCodes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import "./styles/style.scss";
-
-export const randomHexa = () => {
+const randomHexa = () => {
   let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  console.log("#" + n.slice(0, 6));
   return "#" + n.slice(0, 6);
-  console.log(random_hex_color_code());
 };
+window.randomizeColorCodes = () => {
+  const colorCodes = document.querySelectorAll(".color_code");
+  colorCodes.forEach((code) => {
+    code.textContent = randomHexa();
+  });
+};
+
+window.addEventListener("load", updateColorCodes);
